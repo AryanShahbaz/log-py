45dbb1549987f89875667970d60055c4fc320454
Aryan Shahbaz <aryanshahbaz86@gmail.com>
2025-07-04 12:54:17 +0330

    Update log_hello.py

--- log_hello.py
+++ log_hello.py
@@ -1,5 +1,13 @@
+import time
 from datetime import datetime
+import pytz
 
-with open("log.txt", "a", encoding="utf-8") as f:
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    f.write(f"{now} - Hello World\n")
+
+tehran = pytz.timezone("Asia/Tehran")
+
+while True:
+    now = datetime.now(tehran).strftime("%Y-%m-%d %H:%M:%S")
+    with open("log.txt", "a", encoding="utf-8") as f:
+        f.write(f"{now} - Hello World\n")
+    # break
+    time.sleep(60)
